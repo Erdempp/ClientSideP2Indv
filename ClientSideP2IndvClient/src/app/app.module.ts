b836f034f27b3a7795d7ad07ec2592856892cebf
Erdem Pekgüzel <epekguze@avans.nl>
2018-12-10 01:27:01 +0100

    Initialized restaurant

--- ClientSideP2IndvClient/src/app/app.module.ts
+++ ClientSideP2IndvClient/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'restaurant', component: RestaurantComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
